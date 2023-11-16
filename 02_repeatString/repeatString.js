6eef8a8909fc14a15284c47a11385844fc4c2be9
Godfred Kwesi Agyeman <godfredjohn5@gmail.com>
2023-11-16 13:19:11 +0000

    Looped string output, and unskipped tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+let word ="hey";
+let number ;
+const repeatString = function(word,number) {
+    result = "";
+    while(number >=1){
+        result +=word;
+        number--;
+    }
+    if (number <0){
+        result = "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
